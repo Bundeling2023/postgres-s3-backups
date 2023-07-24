6f3cad8fdc35b92a0f519ef1ec17d5cc62e7b601
Nick Nijenhuis <nicknijenhuis@gmail.com>
2023-07-25 01:57:32 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,19 @@ import { CronJob } from "cron";
 import { backup } from "./backup";
 import { env } from "./env";
 
-const job = new CronJob(env.BACKUP_CRON_SCHEDULE, async () => {
+const tryBackup = async () => {
   try {
     await backup();
   } catch (error) {
     console.error("Error while running backup: ", error)
   }
+}
+
+const job = new CronJob(env.BACKUP_CRON_SCHEDULE, async () => {
+  await tryBackup();
 });
 
+tryBackup();
 job.start();
 
 console.log("Backup cron scheduled...")
