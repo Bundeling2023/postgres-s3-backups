30197d10def98baa87f341f9eee6a9a31a4ed9e2
Bundeling <bundeling@2xn.nl>
2023-07-25 02:14:59 +0200

    Run on startup config

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,9 @@ const job = new CronJob(env.BACKUP_CRON_SCHEDULE, async () => {
   await tryBackup();
 });
 
-tryBackup();
+if(env.RUN_ON_STARTUP) {
+  tryBackup();
+}
 job.start();
 
 console.log("Backup cron scheduled...")
